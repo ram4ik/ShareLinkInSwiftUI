69ad8e0e20e98bf6390f99fa17ccfabfef59675c
Ramill <ramill_ibragimov@icloud.com>
2022-10-02 11:02:53 +0300

    Initial Commit

---
+++ ShareLinkInSwiftUI/ShareLinkInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLinkInSwiftUIApp.swift
+//  ShareLinkInSwiftUI
+//
+//  Created by Ramill Ibragimov on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShareLinkInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
